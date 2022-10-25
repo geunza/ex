6037696290dfaf2647e9c7376d323fa99839bceb
geunza <65527146+geunza@users.noreply.github.com>
2022-10-25 18:27:49 +0900

    221025

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
-import React from "react";
-const Home = () => {
+import React, { useEffect, useState } from "react";
+import CountArea from "components/Home/CountArea";
+import SnsLogin from "components/Home/LoginArea";
+import Banner from "components/Home/Banner";
+import Event from "components/Home/Event";
+import styles from "scss/pages/Home.module.scss";
+const Home = ({ isLoggedIn }) => {
   return (
     <>
-      <div>HOME</div>
+      <div className={styles.Home}>
+        <CountArea />
+        <section className={styles.sec01}>
+          <SnsLogin isLoggedIn={isLoggedIn} />
+          <Banner />
+          <Event />
+        </section>
+      </div>
     </>
   );
 };
