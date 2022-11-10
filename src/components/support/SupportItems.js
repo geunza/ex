4557d718fb8fba413c8cb4b904f8207a2411bc56
geunyoung-sbs <safa940812@naver.com>
2022-11-11 01:55:21 +0900

    221111

--- src/components/support/SupportItems.js
+++ src/components/support/SupportItems.js
@@ -3,7 +3,14 @@ import styles from "scss/components/support/SupportItems.module.scss";
 const SupportItems = () => {
   return (
     <>
-      <div className="SupportItems">SupportItems</div>
+      <div className="supportItems">
+        <div className="ordArea">
+          <button type="button">전체</button>
+          <button type="button">인기순</button>
+          <button type="button">금액높은순</button>
+          <button type="button">마감임박순</button>
+        </div>
+      </div>
     </>
   );
 };
