7cd3b2c2364f88b5dfccf5b47f10dccb19b27f6a
geunza <65527146+geunza@users.noreply.github.com>
2023-01-20 16:27:32 +0900

    로컬 로그인 데이터

--- src/pages/login/LoginData.js
+++ src/pages/login/LoginData.js
@@ -1,8 +1,7 @@
 export const KAKAO_REST_API_KEY = "77d36012366dbdc783d9759eb4ee07a3"; // 엑시토
-export const KAKAO_REDIRECT_URI = "https://210.114.19.202:3000/KakaoLogin";
+export const KAKAO_REDIRECT_URI = "https://localhost:3000/KakaoLogin";
 export const KAKAO_CLIENT_SECRET = "rChYQ2C41s1NR7PbAXtcU6G8oCzSQ1nc";
-export const KAKAO_LOGOUT_REDIRECT_URI =
-  "https://210.114.19.202:3000/KakaoLogOut";
+export const KAKAO_LOGOUT_REDIRECT_URI = "https://localhost:3000/KakaoLogOut";
 
 export const APPLE_CLIENT_ID = "kr.co.exitobiz";
 export const APPLE_REDIRECT_URI = "/AppleLogin";
